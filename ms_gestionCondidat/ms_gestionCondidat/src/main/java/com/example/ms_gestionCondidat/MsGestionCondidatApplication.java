3291dcf02b56d7f16ca3140c637b29d6d50d0b05
Rama <rahma.oueslati@esprit.tn>
2023-06-10 11:02:26 +0100

    tpDocker

--- ms_gestionCondidat/ms_gestionCondidat/src/main/java/com/example/ms_gestionCondidat/MsGestionCondidatApplication.java
+++ ms_gestionCondidat/ms_gestionCondidat/src/main/java/com/example/ms_gestionCondidat/MsGestionCondidatApplication.java
@@ -2,8 +2,10 @@ package com.example.ms_gestionCondidat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsGestionCondidatApplication {
 
 	public static void main(String[] args) {
