f6bfeaf1e5889bb1348c0f564973a72bbb7cba9f
Manel03 <manel.hamrouny@gmail.com>
2023-07-01 11:23:16 +0100

    "Eureka config "

---
+++ src/main/java/com/example/ms_gestionCondidat/MsGestionCondidatApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_gestionCondidat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionCondidatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionCondidatApplication.class, args);
+	}
+
+}
